20af9bf2434ab77daeebb709f7ce2c05e3e87b9b
Pavlo Malynin <pavlo@malynin.ca>
2019-03-22 12:42:50 -0600

    Asn

--- sockets.py
+++ sockets.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -25,6 +25,26 @@ import os
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+
+clients = list()
+
+def send_all(msg, c):
+    for client in clients:
+        if client is not c:
+            client.put( msg )
+
+def send_all_json(obj, client):
+    send_all( json.dumps(obj), client )
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
 
 class World:
     def __init__(self):
@@ -45,10 +65,21 @@ class World:
         self.space[entity] = data
         self.update_listeners( entity )
 
-    def update_listeners(self, entity):
+    def fastMerge(self, update, client):
+        # print(update)
+        self.space = {
+            **self.space,
+            **update
+        }
+        
+        # print(self.space)
+        for key in update.keys():
+            self.update_listeners(key, client)
+
+    def update_listeners(self, entity, client = None):
         '''update the set listeners'''
         for listener in self.listeners:
-            listener(entity, self.get(entity))
+            listener(entity, self.get(entity), client)
 
     def clear(self):
         self.space = dict()
@@ -61,27 +92,53 @@ class World:
 
 myWorld = World()        
 
-def set_listener( entity, data ):
-    ''' do something with the update ! '''
+def set_listener( entity, data, client = None):
+    if "path" in data:
+        send_all_json({
+            "{0}".format(entity): data
+        }, client)
+    else:
+        send_all_json({
+            "{0}".format(entity): data
+        }, None)
 
 myWorld.add_set_listener( set_listener )
         
-@app.route('/')
+@app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect("/static/index.html")
 
-def read_ws(ws,client):
+def read_ws(ws, client):
     '''A greenlet function that reads from the websocket and updates the world'''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            msg = ws.receive()
+            if (msg is not None):
+                packet = json.loads(msg)
+                myWorld.fastMerge(packet, client)
+            else:
+                break
+    except:
+        '''Done'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client)    
+    try:
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print(e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,26 +156,36 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    if request.method == "POST":
+        myWorld.update(entity, data)
+    else:
+        myWorld.set(entity, data)
+    return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    if request.method == "POST":
+        data = flask_post_json()
+        myWorld.merge(data)
+    return jsonify(myWorld.space)
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
+    return jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.space)
 
-
-
+@app.route("/static/<path:path>")
+def send_static(path):
+    return send_from_directory("static", path)
+    
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
         pip install gunicorn
